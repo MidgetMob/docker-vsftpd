815c8d744a776aa47e7301153da031daaa9197ea
Jon Schulberger <jschoulzy@gmail.com>
2017-02-16 01:39:46 -0500

    Update vsftpd_init.sh

--- vsftpd_init.sh
+++ vsftpd_init.sh
@@ -23,40 +23,42 @@ chown -R ${VUSER}:${VGRP} /home/${VUSER}
 
 # Enforce config settings for vsftpd
 # TODO: use all ftp ports
-echo "seccomp_sandbox=NO\n" \
-     "listen=YES\n" \
-     "listen_ipv6=NO\n" \
-     "pasv_enable=YES\n" \ 
-     "pasv_address=${pasv_addr}\n" \
-     "pasv_addr_resolve=YES\n" \
-     "anonymous_enable=NO\n"
-     "local_enable=YES\n" \
-     "write_enable=YES\n" \
-     "dirmessage_enable=NO\n" \
-     "use_localtime=YES\n" \
-     "xferlog_enable=YES\n" \
-     "connect_from_port_20=YES\n" \
-     "ascii_upload_enable=YES\n" \
-     "ascii_download_enable=YES\n" \
-     "chroot_local_user=YES\n" \
-     "userlist_deny=NO\n" \
-     "userlist_enable=YES\n" \
-     "userlist_file=/etc/vsftpd/vsftpd.userlist\n" \
-     "secure_chroot_dir=/var/run/vsftpd/empty\n" \
-     "pam_service_name=vsftpd\n" \
-     "rsa_cert_file=${rsa_cert}\n" \
-     "rsa_private_key_file=${rsa_key}\n" \
-     "ssl_enable=YES\n" \
-     "force_local_data_ssl=NO\n" \
-     "force_local_logins_ssl=NO\n" \
-     "ssl_tlsv1=YES\n" \
-     "ssl_sslv2=NO\n" \
-     "ssl_sslv3=NO\n" \
-     "ssl_ciphers=HIGH\n"
+printf \
+"seccomp_sandbox=NO\n
+listen=YES\n
+listen_ipv6=NO\n
+pasv_enable=YES\n" 
+pasv_address=${pasv_addr}\n
+pasv_addr_resolve=YES\n
+anonymous_enable=NO\n
+local_enable=YES\n
+write_enable=YES\n
+dirmessage_enable=NO\n
+use_localtime=YES\n
+xferlog_enable=YES\n
+connect_from_port_20=YES\n
+ascii_upload_enable=YES\n
+ascii_download_enable=YES\n
+chroot_local_user=YES\n
+userlist_deny=NO\n
+userlist_enable=YES\n
+userlist_file=/etc/vsftpd/vsftpd.userlist\n
+secure_chroot_dir=/var/run/vsftpd/empty\n
+pam_service_name=vsftpd\n
+rsa_cert_file=${rsa_cert}\n
+rsa_private_key_file=${rsa_key}\n
+ssl_enable=YES\n
+force_local_data_ssl=NO\n
+force_local_logins_ssl=NO\n
+ssl_tlsv1=YES\n
+ssl_sslv2=NO\n
+ssl_sslv3=NO\n
+ssl_ciphers=HIGH\n"
 
 # Enforce environment variables in config
-echo "pasv_min_port=${port_pasv_min}\n" \
-     "pasv_max_port=${port_pasv_max}\n" \
+printf \
+"pasv_min_port=${port_pasv_min}\n
+pasv_max_port=${port_pasv_max}\n"
 
 # Start vsftpd
 /usr/sbin/vsftpd /etc/vsftpd/vsftpd.conf
