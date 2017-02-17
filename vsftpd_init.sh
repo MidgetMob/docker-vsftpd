b6b2ace08955300c808f54d2361a6248e652f806
Jon Schulberger <jschoulzy@gmail.com>
2017-02-17 01:53:43 -0500

    Update vsftpd_init.sh

--- vsftpd_init.sh
+++ vsftpd_init.sh
@@ -22,46 +22,50 @@ chmod u=rx,g=rx,o=rx /home/${VUSER}
 chown -R ${VUSER}:${VGRP} /home/${VUSER}
 
 # Which config should we use?
-if [ ${custom_conf} != "true" ] || [ ${custom_conf_loc} == "" ]; then
+if [ "$(echo \"${custom_conf}\" | tr '[:upper:]' '[:lower:]')" != "true" ] || [ ${custom_conf_loc} == "" ]; then
   # Enforce config settings for vsftpd
   # TODO: use all ftp ports
   printf \
-"seccomp_sandbox=NO\n
-listen=YES\n
-listen_ipv6=NO\n
-pasv_enable=YES\n
-pasv_min_port=${port_pasv_min}\n
-pasv_max_port=${port_pasv_max}\n
-pasv_address=${pasv_addr}\n
-pasv_addr_resolve=${pasv_addr_resolve}\n
-anonymous_enable=NO\n
-local_enable=YES\n
-write_enable=YES\n
-dirmessage_enable=NO\n
-use_localtime=YES\n
-xferlog_enable=YES\n
-connect_from_port_20=YES\n
-ascii_upload_enable=YES\n
-ascii_download_enable=YES\n
-chroot_local_user=YES\n
-userlist_deny=NO\n
-userlist_enable=YES\n
-userlist_file=/etc/vsftpd/vsftpd.userlist\n
-secure_chroot_dir=/var/run/vsftpd/empty\n
-pam_service_name=vsftpd\n" > ${default_conf_loc}
+"seccomp_sandbox=NO
+listen=YES
+listen_ipv6=NO
+anonymous_enable=NO
+local_enable=YES
+write_enable=YES
+dirmessage_enable=NO
+use_localtime=YES
+xferlog_enable=YES
+connect_from_port_20=YES
+ascii_upload_enable=YES
+ascii_download_enable=YES
+chroot_local_user=YES
+userlist_deny=NO
+userlist_enable=YES
+userlist_file=/etc/vsftpd/vsftpd.userlist
+secure_chroot_dir=/var/run/vsftpd/empty
+pam_service_name=vsftpd\n\n" > ${default_conf_loc}
+
+  # Add passive mode vars if necessary
+  if [ "$(echo \"${pasv_enable}\" | tr '[:upper:]' '[:lower:]')" == "no" ]; then
+    printf \
+"pasv_enable=YES
+pasv_min_port=${port_pasv_min}
+pasv_max_port=${port_pasv_max}
+pasv_address=${pasv_addr}
+pasv_addr_resolve=${pasv_addr_resolve}\n\n" >> ${default_conf_loc}
 
   # Add ssl options if necessary
   if [ "$(echo \"${ssl_enable}\" | tr '[:upper:]' '[:lower:]')" == "yes" ]; then
      printf \
-"ssl_enable=${ssl_enable}\n
-rsa_cert_file=${rsa_cert}\n
-rsa_private_key_file=${rsa_key}\n
-force_local_data_ssl=NO\n
-force_local_logins_ssl=NO\n
-ssl_tlsv1=YES\n
-ssl_sslv2=NO\n
-ssl_sslv3=NO\n
-ssl_ciphers=HIGH" >> ${default_conf_loc}
+"ssl_enable=${ssl_enable}
+rsa_cert_file=${rsa_cert}
+rsa_private_key_file=${rsa_key}
+force_local_data_ssl=NO
+force_local_logins_ssl=NO
+ssl_tlsv1=YES
+ssl_sslv2=NO
+ssl_sslv3=NO
+ssl_ciphers=HIGH\n\n" >> ${default_conf_loc}
   fi
 # Copy the custom config to the default location
 else
